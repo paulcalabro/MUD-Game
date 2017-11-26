8b4e25f55613599edd07d0ed7868d3a696fade70
Rene Palomo <renepalomo@Renes-MacBook-Pro.local>
2017-11-25 19:41:04 -0700

    hey paul no changes

--- character.py
+++ character.py
@@ -1,4 +1,7 @@
-#TOOD: Add character related code here...
+#TOOD: Add character related code here.
 
 
 # @Rene: Please add a method to set the character name.
+
+
+# @Paul: Pleast let me know if I pushed this correctly
